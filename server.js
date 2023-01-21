a6ed779092fbfe58a6fa155e57a2a3fc8032f3e7
Ashish Gupta <gashish4950@gmail.com>
2023-01-21 09:29:53 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 connectDB();
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server Working on ${PORT}`);
